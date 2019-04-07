f848ac8079e51358bf0d7c60e45aa9ea954bb54e
Alexander Knowles <alexander.knowles@gmail.com>
2019-04-06 22:21:59 -0500

    Configure Phoenix to use Jason library for JSON

--- apps/web/config/config.exs
+++ apps/web/config/config.exs
@@ -4,16 +4,16 @@
 # the umbrella root.
 use Mix.Config
 
-# General application configuration
 config :web,
   generators: [context_app: false]
 
-# Configures the endpoint
 config :web, Web.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "9B1xcmVz3ZjcROqaTcTuwNJ+dsy3+FGf5+861wxoI7uqMagqrU5Sk2sBZT8Zk2vd",
   render_errors: [view: Web.ErrorView, accepts: ~w(json)],
   pubsub: [name: Web.PubSub, adapter: Phoenix.PubSub.PG2]
+
+config :phoenix, :json_library, Jason
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
