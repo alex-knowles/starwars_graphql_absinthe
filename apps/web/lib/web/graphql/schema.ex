5b4d03b39d850daca00e7edca879f2d050fdec26
Alexander Knowles <alexander.knowles@gmail.com>
2019-04-07 00:52:19 -0500

    Define Human object type

--- apps/web/lib/web/graphql/schema.ex
+++ apps/web/lib/web/graphql/schema.ex
@@ -39,4 +39,25 @@ defmodule Web.GraphQL.Schema do
     @desc "The droid's primary function."
     field(:primary_function, :string)
   end
+
+  @desc "A humanoid creature in the Star Wars universe."
+  object :human do
+    @desc "The ID of the human."
+    field(:id, non_null(:string))
+
+    @desc "The name of the human."
+    field(:name, :string)
+
+    @desc "The friends of the human, or an empty list if the have none."
+    field(:friends, list_of(:human))
+
+    @desc "Which movies the human appears in."
+    field(:appearsIn, list_of(:episode))
+
+    @desc "Where are they from and how they came to be who they are."
+    field(:secret_backstory, :string)
+
+    @desc "The home planet of the human, of null if unknown."
+    field(:home_planet, :string)
+  end
 end
