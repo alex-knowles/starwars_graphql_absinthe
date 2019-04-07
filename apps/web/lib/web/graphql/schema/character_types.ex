04c99d393e28438cd83bd9927857f428c0831034
Alexander Knowles <alexander.knowles@gmail.com>
2019-04-07 17:13:32 -0500

    Define Character interface
    Implemented by:
    * Human
    * Droid

--- apps/web/lib/web/graphql/schema/character_types.ex
+++ apps/web/lib/web/graphql/schema/character_types.ex
@@ -4,16 +4,42 @@ defmodule Web.GraphQL.Schema.CharacterTypes do
   """
   use Absinthe.Schema.Notation
 
+  @desc "A character in the Star Wars Trilogy"
+  interface :character do
+    @desc "The ID of the character."
+    field(:id, non_null(:string))
+
+    @desc "The name of the character."
+    field(:name, :string)
+
+    @desc "The friends of the character, or an empty list if they have none."
+    field(:friends, list_of(:character))
+
+    @desc "Which movies the character appears in."
+    field(:appears_in, list_of(:episode))
+
+    @desc "All secrets about their past."
+    field(:secret_backstory, :string)
+
+    resolve_type(fn
+      %{character_type: :droid}, _ -> :droid
+      %{character_type: :human}, _ -> :human
+      _, _ -> nil
+    end)
+  end
+
   @desc "A mechanical creature in the Star Wars universe."
   object :droid do
+    interfaces([:character])
+
     @desc "The ID of the droid."
     field(:id, non_null(:string))
 
     @desc "The name of the droid."
     field(:name, :string)
 
-    @desc "The friends of the droid, or an empty list if the have none."
-    field(:friends, list_of(:droid))
+    @desc "The friends of the droid, or an empty list if they have none."
+    field(:friends, list_of(:character))
 
     @desc "Which movies the droid appears in."
     field(:appears_in, list_of(:episode))
@@ -27,14 +53,16 @@ defmodule Web.GraphQL.Schema.CharacterTypes do
 
   @desc "A humanoid creature in the Star Wars universe."
   object :human do
+    interfaces([:character])
+
     @desc "The ID of the human."
     field(:id, non_null(:string))
 
     @desc "The name of the human."
     field(:name, :string)
 
-    @desc "The friends of the human, or an empty list if the have none."
-    field(:friends, list_of(:human))
+    @desc "The friends of the human, or an empty list if they have none."
+    field(:friends, list_of(:character))
 
     @desc "Which movies the human appears in."
     field(:appears_in, list_of(:episode))
