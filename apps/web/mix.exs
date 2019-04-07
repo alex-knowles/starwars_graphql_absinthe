f77d92df6abd240edda61b5fef7b992862a71324
Alexander Knowles <alexander.knowles@gmail.com>
2019-04-07 15:34:22 -0500

    Add 'core' to list of 'web' dependencies

--- apps/web/mix.exs
+++ apps/web/mix.exs
@@ -41,6 +41,7 @@ defmodule Web.MixProject do
       {:absinthe, "~> 1.4.2"},
       {:absinthe_plug, "~> 1.4.0"},
       {:absinthe_phoenix, "~> 1.4.0"},
+      {:core, in_umbrella: true},
       {:credo, "~> 1.0", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0.0-rc.6", only: [:dev, :test], runtime: false},
       {:gettext, "~> 0.11"},
