34319019a3730d5b1fd481c0d8fdb64e8f492208
Alexander Knowles <alexander.knowles@gmail.com>
2019-04-07 00:55:46 -0500

    Define Droid object type

--- apps/web/lib/web/graphql/schema.ex
+++ apps/web/lib/web/graphql/schema.ex
@@ -18,4 +18,25 @@ defmodule Web.GraphQL.Schema do
     value(:empire)
     value(:jedi)
   end
+
+  @desc "A mechanical creature in the Star Wars universe."
+  object :droid do
+    @desc "The ID of the droid."
+    field(:id, non_null(:string))
+
+    @desc "The name of the droid."
+    field(:name, :string)
+
+    @desc "The friends of the droid, or an empty list if the have none."
+    field(:friends, list_of(:droid))
+
+    @desc "Which movies the droid appears in."
+    field(:appearsIn, list_of(:episode))
+
+    @desc "Construction date and the name of the designer."
+    field(:secret_backstory, :string)
+
+    @desc "The droid's primary function."
+    field(:primary_function, :string)
+  end
 end
