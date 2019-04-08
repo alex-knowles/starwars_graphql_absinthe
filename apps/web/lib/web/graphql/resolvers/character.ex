fb4643ac98ecc6ee7ea0ad63f138e42788f54154
Alexander Knowles <alexander.knowles@gmail.com>
2019-04-07 19:21:12 -0500

    Update hero/3 resolver to accept optional 'episode' argument

--- apps/web/lib/web/graphql/resolvers/character.ex
+++ apps/web/lib/web/graphql/resolvers/character.ex
@@ -4,5 +4,7 @@ defmodule Web.GraphQL.Resolvers.Character do
   """
 
   @spec hero(map(), map(), Absinthe.Resolution.t()) :: {:ok, map()}
-  def hero(_parent, _args, _resolution), do: {:ok, Core.hero()}
+  def hero(_parent, args, resolution)
+  def hero(_, %{episode: episode}, _), do: {:ok, Core.hero(episode)}
+  def hero(_, _, _), do: {:ok, Core.hero()}
 end
