86094ed3bd5da1ddfd6c3772fe925594151e7ebb
Alexander Knowles <alexander.knowles@gmail.com>
2019-04-14 22:26:36 -0500

    Add Core.characters_by_id/1

--- apps/core/lib/core.ex
+++ apps/core/lib/core.ex
@@ -47,4 +47,17 @@ defmodule Core do
       _ -> nil
     end
   end
+
+  @doc """
+  Return a batch of Star Wars Characters.
+
+  Given a list of IDs, returns a list of Characters that match.
+  """
+  @spec characters_by_id([String.t()]) :: [map()]
+  def characters_by_id(ids) do
+    ids
+    |> Enum.uniq()
+    |> Repo.characters_by_id()
+    |> Enum.reject(&is_nil/1)
+  end
 end
